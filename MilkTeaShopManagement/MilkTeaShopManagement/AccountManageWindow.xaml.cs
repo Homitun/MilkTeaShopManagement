eb4813976b752848addd88208c9ff0cfb7246d2c
Punnyanfa <phamvantrieuthuan12345@gmail.com>
2024-08-18 10:25:04 +0700

    add

--- MilkTeaShopManagement/MilkTeaShopManagement/AccountManageWindow.xaml.cs
+++ MilkTeaShopManagement/MilkTeaShopManagement/AccountManageWindow.xaml.cs
@@ -28,5 +28,25 @@ namespace MilkTeaShopManagement
         {
 
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
