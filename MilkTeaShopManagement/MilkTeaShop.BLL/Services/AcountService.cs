33928c212111581a599d8203ccb80a3577d71dae
Punnyanfa <phamvantrieuthuan12345@gmail.com>
2024-08-22 10:43:06 +0700

    last push

---
+++ MilkTeaShopManagement/MilkTeaShop.BLL/Services/AcountService.cs
@@ -0,0 +1,32 @@
+﻿using MilkTeaShop.DAL.Models;
+using MilkTeaShop.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaShop.BLL.Services
+{
+    public class AcountService
+    {
+        private AccountRepo _repo = new();
+        public List<Account> GetAllAccount()
+        {
+            return _repo.GetAll();
+        }
+        public void AddAccount(Account account)
+        {
+            _repo.Add(account);
+        }
+        public void UpdateAccount(Account account)
+        {
+            _repo.Update(account);
+        }
+        public void RemoveAccount(Account account)
+        {
+            _repo.Delete(account);
+        }
+
+    }
+}
