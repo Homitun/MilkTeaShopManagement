6388e1f972232ab4ae95873533ff90f416d9c2b1
Homitun <hoangtuan221001@gmail.com>
2024-08-22 20:14:39 +0700

    Create function login

---
+++ MilkTeaShopManagement/MilkTeaShop.BLL/Services/MemberService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MilkTeaShop.DAL.Models;
+using MilkTeaShop.DAL.Repositories;
+
+namespace MilkTeaShop.BLL.Services
+{
+    public class MemberService
+    {
+        private MemberRepository _repo = new();
+
+        public Account Authenticate(String username, String password)
+        {
+            return _repo.GetOne(username, password);
+
+        }
+    }
+}
---
+++ MilkTeaShopManagement/MilkTeaShop.DAL/Repositories/MemberRepository.cs
@@ -0,0 +1,29 @@
+﻿using MilkTeaShop.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaShop.DAL.Repositories
+{
+
+    //GUI-UI<-> SERVICE<-> REPO <-> DBCONTEXT <-> TABLE THUWCJ SUWJ OWR SQL SERVER, MY SQL
+    //L1         L2        L3
+    //UI         BLL       DAL CURD TABLE
+    public class MemberRepository
+    {
+        private MilkTeaShopManagementContext _context;// khoong new mooix laanf xaif 1 laanf new
+
+        public Account? GetOne(string userName, string password)
+        {
+            _context = new();
+            //return _context.Account.ToList(); trar heet
+            return _context.Accounts.FirstOrDefault(x => x.UserName.ToLower() == userName.ToLower() && x.PassWord == password);
+            // hoac tra ve 1 dong hoac tra ve null, tim khong thay email hoac passs
+
+        }
+
+
+    }
+}
