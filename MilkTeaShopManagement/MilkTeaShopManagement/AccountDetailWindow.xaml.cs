26d933b57051e3e6202d6cb9d9cf78f2ab9d2682
Punnyanfa <phamvantrieuthuan12345@gmail.com>
2024-08-22 10:14:35 +0700

    re push code

---
+++ MilkTeaShopManagement/MilkTeaShopManagement/AccountDetailWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using MilkTeaShop.BLL.Services;
+using MilkTeaShop.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MilkTeaShopManagement
+{
+    /// <summary>
+    /// Interaction logic for AccountDetai_Window.xaml
+    /// </summary>
+    public partial class AccountDetai_Window : Window
+    {
+        private AcountService _service = new();
+        public Account EditedAccount { get; set; } = null;  // flag 
+        public AccountDetai_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Account account = new();
+            account.UserName = userNameTexbox.Text;
+            account.DisplayName = DisplayNameTexbox.Text;
+            account.PassWord = PasswordTexbox.Text;
+            account.Type = int.Parse(TypeTexbox.Text);
+
+            if(EditedAccount == null)
+                _service.AddAccount(account);
+            else
+                _service.UpdateAccount(account);
+                    this.Close();
+        }
+        private void FillElement(Account account)
+        {
+            if (account == null)
+                return;
+            userNameTexbox.Text = account.UserName;
+            userNameTexbox.IsEnabled = false;
+            DisplayNameTexbox.Text = account.DisplayName;
+            PasswordTexbox.Text = account.PassWord;
+            TypeTexbox.Text = account.Type.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillElement(EditedAccount);
+            if(EditedAccount == null)
+            {
+                LabelDetailMode.Content = "Create a new member";
+            }
+            else
+            {
+                LabelDetailMode.Content = "Update member's infomation";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ MilkTeaShopManagement/MilkTeaShopManagement/AccountManagerWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using MilkTeaShop.BLL.Services;
+using MilkTeaShop.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MilkTeaShopManagement
+{
+    /// <summary>
+    /// Interaction logic for AccountManagerWindow.xaml
+    /// </summary>
+    public partial class AccountManagerWindow : Window
+    {
+        private AcountService _service = new();
+        public AccountManagerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillDataGrid();
+        }
+        private void FillDataGrid()
+        {
+            AccountGrid.ItemsSource = null;
+            AccountGrid.ItemsSource = _service.GetAllAccount();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AccountDetai_Window detail = new AccountDetai_Window();
+            detail.ShowDialog();
+            FillDataGrid();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Account? selected = AccountGrid.SelectedItem as Account;
+            if (selected == null)
+            {
+                MessageBox.Show("Please seclect 1 account before editing","Select an Account", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            AccountDetai_Window accountDetai_Window = new AccountDetai_Window();
+            accountDetai_Window.EditedAccount = selected;
+            accountDetai_Window.ShowDialog();
+            FillDataGrid();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Account? selected = AccountGrid.SelectedItem as Account;
+            if (selected == null)
+            {
+                MessageBox.Show("Please seclect 1 account before deleting", "Select an Account", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            MessageBoxResult confirm = MessageBox.Show("Do you really wanna remove this account ?", "Confirm ?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (confirm == MessageBoxResult.No)
+                return;
+            else
+                _service.RemoveAccount(selected);
+            FillDataGrid();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
