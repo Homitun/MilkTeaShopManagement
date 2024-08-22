e4b35850e98838b48f8aab9e05582667a713412e
HDuc85 <hoangduc8501@gmail.com>
2024-08-22 21:40:01 +0700

    CRUD_Table
    Table Manage view
    Repo Table
    Service Table

---
+++ MilkTeaShopManagement/MilkTeaShop.BLL/Repository/TableDrinkRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilkTeaShop.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaShop.DAL.Repository
+{
+    public class TableDrinkRepository
+    {
+        private MilkTeaShopManagementContext _context;
+
+        public List<TableDrink> GetAll()
+        {
+            _context = new MilkTeaShopManagementContext();
+            return _context.TableDrinks.ToList();
+        }
+        public List<TableDrink> GetAllActiveTable()
+        {
+            _context = new MilkTeaShopManagementContext();
+            return _context.TableDrinks.Where(x => x.Status == "active").ToList();
+        }
+        public List<TableDrink> GetAllInactiveTable()
+        {
+            _context = new MilkTeaShopManagementContext();
+            return _context.TableDrinks.Where(x => x.Status == "inactive").ToList();
+        }
+        public TableDrink GetById(int id)
+        {
+            _context = new MilkTeaShopManagementContext();
+            return _context.TableDrinks.FirstOrDefault(x => x.Id == id);
+        }
+        public int Add(TableDrink tableDrink)
+        {
+            _context = new MilkTeaShopManagementContext();
+            _context.TableDrinks.Add(tableDrink); 
+            _context.SaveChanges();
+            return tableDrink.Id;
+        }
+
+        public void Update(TableDrink tableDrink)
+        {
+            _context = new MilkTeaShopManagementContext();
+            _context.TableDrinks.Update(tableDrink);
+            _context.SaveChanges();
+        }
+
+        public void Delete(TableDrink tableDrink)
+        {
+            _context = new MilkTeaShopManagementContext();
+            _context.TableDrinks.Remove(tableDrink);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ MilkTeaShopManagement/MilkTeaShop.BLL/Services/TableService.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using MilkTeaShop.DAL.DTO;
+using MilkTeaShop.DAL.Models;
+using MilkTeaShop.DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaShop.BLL.Services
+{
+    public class TableService
+    {
+        private TableDrinkRepository _tableDrinkRepository = new();
+        private BillRepository _billRepository = new();
+        private BillInfoRepository _billInfoRepository = new();
+        private DrinkRepository _drinkRepository = new();
+
+        public List<Drink> GetAllDrinks() 
+        {
+            return _drinkRepository.GetAll();
+        }
+
+        public BillInforView GetBillWithId(int billId)
+        {
+            return _billInfoRepository.GetBillInfoWithId(billId);
+        }
+      
+
+        public void AddDrink (int billId, int drinkId, int quantity)
+        {
+            var billInfo = _billInfoRepository.GetWithCondition(x => x.IdBill == billId);
+            if (billInfo == null)
+            {
+                _billInfoRepository.Add(new BillInfo
+                {
+                    IdBill = billId,
+                    Count = quantity,
+                    IdDrink = drinkId,
+                });
+            }
+            else 
+            {
+                _billInfoRepository.UpdateQuantity(billId, drinkId, quantity);
+            }
+        }
+
+        public int AddTable()
+        {
+           
+           var tabledrinkid = _tableDrinkRepository.Add(new TableDrink
+            {
+               Name = "Table " + _tableDrinkRepository.GetAll().Count,
+               Status = "active"
+           });
+
+             return _billRepository.Add(new Bill
+            {
+                IdTable = tabledrinkid,
+                DateCheckIn = DateOnly.FromDateTime(DateTime.Now),
+                Status = 0
+            });
+
+        }
+        public void SubtractDrink(int billId, int drinkId, int quantity)
+        {
+            _billInfoRepository.UpdateQuantity(billId, drinkId, -quantity);
+        }
+        public void RemoveDrink (int billId, int drinkId)
+        {
+           var billInfos = _billInfoRepository.GetWithCondition(x => x.IdBill == billId && x.IdDrink == drinkId);
+            if(billInfos != null)
+            {
+                _billInfoRepository.Delete(billInfos.First());
+            }
+        }
+        public void UpdateTableName(int billId, string tableName)
+        {
+            var bill = _billRepository.GetWithCondition(x => x.Id == billId);
+            var table = _tableDrinkRepository.GetById(bill.First().IdTable);
+            if(table != null)
+            {
+                table.Name = tableName;
+            _tableDrinkRepository.Update(table);
+            }
+        }
+        public List<Bill> GetAllBillActive()
+        {
+            return _billRepository.GetAllWithstatus0();
+        }
+        public void PaymentBill(int billId)
+        {
+            _billRepository.UpdateStatus(billId, 1);
+            var bill = _billRepository.GetWithCondition(x => x.Id == billId);
+            var table = _tableDrinkRepository.GetById(bill.First().IdTable);
+            table.Status = "Finished";
+            _tableDrinkRepository.Update(table);
+        }
+        public void DeleteBill(int billId)
+        {
+            var bill = _billRepository.GetWithCondition(x => x.Id == billId);
+            _billRepository.Delete(bill.First());
+            var table = _tableDrinkRepository.GetById(bill.First().IdTable);
+            _tableDrinkRepository.Delete(table);
+        }
+        public List<Drink> SearchWithCate(int cateId)
+        {
+            return _drinkRepository.SortWithCate(cateId);
+        }   
+        public List<Drink> SearchDrink(string keywork)
+        {
+            return _drinkRepository.Search(keywork);
+        }
+    }
+}
